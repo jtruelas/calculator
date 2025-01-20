c9d448ca30d401c570909892c80b63fa20181613
jtruelas <joshuatruelas@gmail.com>
2025-01-20 00:33:16 -0800

    Added a function that call on the basic math functions based on operator selected

--- functions.js
+++ functions.js
@@ -6,7 +6,13 @@ function multiply(num1, num2) {return num1 * num2;}
 
 function divide(num1, num2) {return num1 / num2;}
 
-console.log(add(2, 3));
-console.log(subtract(10, 7));
-console.log(multiply(3, 4));
-console.log(divide(10, 5));
+let num1;
+let num2;
+let sign;
+
+function operate(sign, num1, num2) {
+    return sign == '+' ? add(num1, num2) : 
+    sign == '-' ? subtract(num1, num2) :
+    sign == '*' ? multiply(num1, num2) :
+    sign == '/' ? divide(num1, num2) : 'Error';
+}
