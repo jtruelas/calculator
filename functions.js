34b4b8a6e61c7a481e07f84df24e8b6a1d74c86a
jtruelas <joshuatruelas@gmail.com>
2025-01-20 00:19:04 -0800

    Created base functions for add, subtract, multiply and divide

---
+++ functions.js
@@ -0,0 +1,12 @@
+function add (num1, num2) {return num1 + num2;}
+
+function subtract(num1, num2) {return num1 - num2;}
+
+function multiply(num1, num2) {return num1 * num2;}
+
+function divide(num1, num2) {return num1 / num2;}
+
+console.log(add(2, 3));
+console.log(subtract(10, 7));
+console.log(multiply(3, 4));
+console.log(divide(10, 5));
